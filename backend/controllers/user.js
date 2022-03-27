d2abac7b64b29b4c5fda8f793ae0880ede4a71b2
master <master.code.develop@gmail.com>
2022-03-27 03:13:38 +0200

    v5

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -14,7 +14,7 @@ exports.signup = (req, res, next) => {
         if(results.length == 0) {
             bcrypt.hash(req.body.password, 10)
             .then(hash => {
-                const user ={
+                const user = {
                     email: req.body.email,
                     password: hash
                 };
@@ -43,44 +43,59 @@ exports.signup = (req, res, next) => {
             })
         }
     });
-    /*
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user ={
-                email: req.body.email,
-                password: hash
-            };
-        })
-        .catch(error => res.status(500).json({ error }));
-    */
+}
 
-    /*
-    
+//Connexion au compte de l'utilisateur 
+exports.login = (req, res, next) => {
+    connection.query('SELECT * FROM `users` WHERE `email` = "'+req.body.email+'"', (error, results, fields) => {
+        
+        //S'il y a un compte avec cette email
+        if(results.length == 1) {
+            const user = results[0];
+            
+            bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
 
-        .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-            .then(() => res.status(201).json({  message: "L\'utilisateur a été créé avec succès" }))
-            .catch(error => res.status(401).json({ 
-                error: error,
+                if (!valid) {
+                    return res.status(401).json({ message: 'le mot de passe incorrect' });
+                }
+                const token = jwt.sign({},'${process.env.TOKEN}',{ expiresIn: '24h' })
+                connection.query('UPDATE `users` SET `token` = "'+token+'" ', (error, results, fields) => {
+
+                    if(error == null){
+                        res.status(201).json({ 
+                            message: {
+                                email: "",
+                                password: ""
+                            } 
+                        }) 
+                    }
+                })
+
+            })
+            .catch(error => res.status(500).json({ error }));
+        }
+
+        //S'il n'y a pas de compte avec cette addresse email
+        else if(results.length == 0) {
+            res.status(401).json({ 
                 message: {
                     name: "",
                     firstName: "",
                     email: "Cet email est déjà utilisé, merci de choisir un autre",
                     password: ""
                 } 
-            }));
-        })
-        .catch(error => res.status(500).json({ error }));
-        */
-};
+            })
+        }
 
-//Connexion au compte de l'utilisateur 
-exports.login = (req, res, next) => {
-    console.log('login')
+        //S'il y a plusieurs comptes
+        else {
+            console.log('fatal error: user.js l.55');
+        }
+
+    })
+    
+    /*
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
@@ -105,4 +120,5 @@ exports.login = (req, res, next) => {
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
+        */
 };
