4d9fd5e0690ca43f91bc4790310544c44da41940
master <master.code.develop@gmail.com>
2022-06-24 11:59:58 +0200

    install backend

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+// IMPORTS
+const express = require('express'),
+bodyParser = require('body-parser'),
+path = require('path'),
+app = express();
+
+
+
+
+
+app.use((req, res, next) => {
+    // Permet d'accéder l'API depuis n'importe quelle origine ('*')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Permet d'ajouter les headers mentionnés aux requêtes envoyées vers l'API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    
+    // Permet d'envoyer des requêtes avec les méthodes mentionnées
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    // Renvoie au prochain Middleware
+    next();
+});
+
+app.use(bodyParser.json());
+
+// TEST
+app.use('', (req, res, next) => {
+    res.json({
+        "test": "success"
+    })
+});
+//  exports
+module.exports = app;
